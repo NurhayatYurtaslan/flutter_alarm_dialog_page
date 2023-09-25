d8204cb7406fc837af45f9d682465a87bd4a6e73
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-09-25 22:36:45 +0300

    update: design

--- flutter_alarm_dialog_page/lib/dialog_button.dart
+++ flutter_alarm_dialog_page/lib/dialog_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DialogButton extends StatelessWidget {
-  const DialogButton({super.key});
+  const DialogButton({Key ,key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +11,53 @@ class DialogButton extends StatelessWidget {
       onPressed: () {
         showDialog(
           context: context,
+          barrierDismissible: false, // Prevents dismissing the dialog by tapping outside
           builder: (context) {
-            return CupertinoAlertDialog(
-              title: const Text('Delete Account'),
-              content: const Text(
-                'Are you sure you want to delete your account? This action cannot be undone!',
+            return AlertDialog(
+              shadowColor: Colors.purple,
+              surfaceTintColor: Color.fromARGB(255, 0, 26, 77),
+              scrollable: RenderObject.debugCheckingIntrinsics,
+              title: Text(
+                'Delete Account',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
               ),
+              content: Text(
+                'Are you sure you want to delete your account? This action cannot be undone!',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              backgroundColor: Colors.white,
+              elevation: 8,
               actions: [
-                CupertinoDialogAction(
-                  child: const Text('Cancel'),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  child: Text('Cancel'),
                   onPressed: () => Navigator.of(context).pop(false),
                 ),
-                CupertinoDialogAction(
-                  isDefaultAction: true,
-                  child: const Text('Delete'),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  child: Text('Delete'),
                   onPressed: () => Navigator.of(context).pop(true),
                 ),
               ],
